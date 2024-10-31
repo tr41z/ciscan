b9692a25f664b6599f531f012a8c827121abcf97
traizooo <michal.owsiejew23@gmail.com>
2024-10-31 12:18:46 +0000

    Remove scanner and utils modules; delete associated functions and main logic for cleaner codebase

--- src/include/scanner.h
+++
@@ -1,7 +0,0 @@
-#ifndef SCANNER_H
-#define SCANNER_H
-
-void scan_ports(const char *hostname, int portU, int portL);
-int establish_handshake(const char *hostname, int port);
-
-#endif
--- src/include/utils.h
+++
@@ -1,8 +0,0 @@
-#ifndef UTILS_H
-#define UTILS_H
-
-void display_menu();
-void ascii_title();
-void credits();
-
-#endif
